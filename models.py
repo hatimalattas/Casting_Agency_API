3140d54481680ba10d7178e7d4f693b55dfa1600
Hatim Alattas <56390393+hatimalattas@users.noreply.github.com>
2020-07-29 22:49:38 +0300

    changed app var

--- models.py
+++ models.py
@@ -27,66 +27,67 @@ Movie Model
 
 '''
 class Movie(db.Model):  
-  __tablename__ = 'movies'
+    __tablename__ = 'movies'
 
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String, nullable=False)
-  release_date = db.Column(db.DateTime(), default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    release_date = db.Column(db.DateTime(), default=datetime.utcnow)
 
-  def __init__(self, title, release_date):
-    self.title = title
-    self.release_date = release_date
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def update(self):
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'release_date': self.release_date,
-    }
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date,
+        }
 
 '''
 Actor Model
 
 '''
 class Actor(db.Model):  
-  __tablename__ = 'actors'
+    __tablename__ = 'actors'
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String, nullable=False)
-  age = db.Column(db.Integer, nullable=False)
-  gender = db.Column(db.String, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    gender = db.Column(db.String, nullable=False)
 
-  def __init__(self, name, age, gender):
-    self.name = name
-    self.age = age
-    self.gender = gender
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'age': self.age,
-      'gender': self.gender
-    }
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
